7474e77494d9a59f5f2b890d036794d97a391168
Hetali A <60947831+HetaliAcharya@users.noreply.github.com>
2022-07-11 19:41:50 +0100

    Update Split_String_(split the bill).py

--- Split_String_(split the bill).py
+++ Split_String_(split the bill).py
@@ -1,1 +1,14 @@
+# Split string method
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+# Don't change the code above 
 
+# code below
+import random
+indx = []
+for i in range(len(names)):
+  indx.append(i)
+#print(indx)
+for i in range(len(indx)):
+  name = random.randint(0,(len(indx)-1))
+print(names[name])
